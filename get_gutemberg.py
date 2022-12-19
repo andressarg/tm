5932fb4b57d64bdd03524cee651c40d7fa636a4c
andressa gomide <gomide.andressa@gmail.com>
2022-12-19 13:27:16 +0100

    fiz so pq tive que desligar

--- get_gutemberg.py
+++ get_gutemberg.py
@@ -25,7 +25,7 @@ Functions in this file
 import re # for regular expressions
 from urllib.request import urlopen # to request the content from the internet
 from bs4 import BeautifulSoup # to work with html files (bs4 is known to be user friendly)
-
+import pandas as pd # to store metadata as dataframe
 
 def download_url(urlpath):
     ''' 
@@ -45,85 +45,147 @@ def download_url(urlpath):
         print(f"There was an issue when trying to download{urlpath}")
 
 
-# set the book id
+# create a list with the books id
 
-book_id = "44540" # cinco minutos j de alencar
-book_id = "55682" # quincas borba machado
-book_id = "31971" # o crime do padre amaro eca de queiros
-##########################
+# book_id = "44540" # cinco minutos j de alencar
+# book_id = "55682" # quincas borba machado
+# book_id = "31971" # o crime do padre amaro eca de queiros
 
-# from plain text
-# url for plain text book
-url_plain = f'https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}.txt'
-
-# download the content
-data_plain = download_url(url_plain)
-
-# plain text link doesnt include metadata. we have to go to the previous page
-url_meta = f'https://www.gutenberg.org/ebooks/{book_id}'
-metadata = download_url(url_meta)
-
-# parse document 
-soup = BeautifulSoup(metadata, 'html.parser')
-
-# get metadata
-author = soup.find('a', {'about': re.compile(r'\/authors\/.*')}).text
-lang = soup.find('a', {'href': re.compile(r'\/browse\/languages\/.*')}).text
-subj = soup.find('a', {'href': re.compile(r'\/ebooks\/subject\/*')}).text
-title = soup.find('td', {'itemprop': 'headline'}).text
-datepub = soup.find('td', {'itemprop': 'datePublished'}).text
-
-# write content to file
-with open(f"input/book{book_id}.txt", 'wb') as file:
-    file.write(data_plain)
-
-# TODO
-# write metadata
+book_id_list = ["44540", "55682", "31971"]
 
 
-##########################
-# from html
-url_html = f'https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}-images.html'
-data_html = download_url(url_html)
+'''
+- sometimes the same data content is available in different formats. it is a good idea to test extracting two different formats to get an idea which one will be better for the project.
+- it is almost always easier to work with plain text, but preserving section breaks can lead to further analysis
+- in our case, getting the data from html format sounds better and easier to (a) preserve the sections boundaries (b) to make cleaning easier
+'''
+
+# to get the books from the plain format
+
+# create empty df to store the metadata
+
+df = pd.DataFrame(columns = ['author', 'title', 'lang', 'subj', 'datepub'])
+
+for book_id in book_id_list:
+    # url for plain text book
+    url_plain = f'https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}.txt'
+
+    # download the content
+    data_plain = download_url(url_plain)
+
+    # plain text link doesnt include metadata. 
+    # we have to go to the previous page
+    url_meta = f'https://www.gutenberg.org/ebooks/{book_id}'
+    metadata = download_url(url_meta)
+
+    # parse document 
+    soup = BeautifulSoup(metadata, 'html.parser')
+
+    # get metadata
+    author = soup.find('a', {'about': re.compile(r'\/authors\/.*')}).text
+    lang = soup.find('a', {'href': re.compile(r'\/browse\/languages\/.*')}).text
+    subj = soup.find('a', {'href': re.compile(r'\/ebooks\/subject\/*')}).text
+    title = soup.find('td', {'itemprop': 'headline'}).text
+    datepub = soup.find('td', {'itemprop': 'datePublished'}).text
+
+    # remove line breaks
+    meta_list = [sub.replace('\n', '') for sub in [author, title, lang, subj, datepub]]
+
+
+    # df.loc[book_id] = [book_id, meta_list[0], meta_list[1], meta_list[2], meta_list[3], meta_list[4]]
+    df.loc[book_id] = [meta_list[0], meta_list[1], meta_list[2], meta_list[3], meta_list[4]]
+
+    # write book content to file
+    with open(f"input/plain/{book_id}.txt", 'wb') as file:
+        file.write(data_plain)
+
+
+# write metadata to file
+df.to_csv('output/books_metadata.tsv', sep='\t', encoding='utf-8')
+    
+
+
+
+
+
+# to get the books from html
+# create empty df to store the metadata
+
+df = pd.DataFrame(columns = ['author', 'title', 'lang', 'subj', 'datepub'])
+
+for book_id in book_id_list:
+    url_html = f'https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}-images.html'
+    data_html = download_url(url_html)
+
+    # parse
+    soup = BeautifulSoup(data_html, 'html.parser')
+
+    # get metadata
+    author = soup.find('meta', {'name' : 'AUTHOR'})['content']
+    lang = soup.find('meta', {'name' : 'dc.language'})['content']
+    subj = soup.find('meta', {'name' : 'dc.subject'})['content']
+    title = soup.find('meta', {'property' : 'og:title'})['content']
+    datepub = soup.find('meta', {'name' : 'dcterms.created'})['content']
+
+    ## remove unnecessary elements
+    # style
+    for i in soup.find_all('style'):
+        i.decompose()
+
+    # boiler plates
+    for i in soup.find_all('section', {'class': re.compile('.*boilerplate.*')}):
+        i.decompose()
+
+    # editor comments
+    for i in soup.find_all('div', {'class': 'fbox'}):
+        i.decompose()
+
+    # page numbers
+    for i in soup.find_all('span', {'class': 'pagenum'}):
+        i.decompose()
+
+    # remove br tags
+    for i in soup.find_all('br'):
+        i.unwrap()
+
+    # remove head
+    soup.find('head').decompose()
+
+    # get metadata
+    df.loc[book_id] = [author, title, lang, subj, datepub]
+
+
+    # write to file with tags
+    with open(f'input/{book_id}.html', 'w', encoding = 'utf-8') as file:
+        file.write(str(soup.prettify()))
+    # write to file without tags
+    with open(f'input/{book_id}.txt', 'w', encoding = 'utf-8') as file:
+        file.write(soup.text)
+
+
+
 
 # parse
 soup = BeautifulSoup(data_html, 'html.parser')
 
 # get metadata
-author = soup.find('meta', {'name' : 'AUTHOR'})['content']
-lang = soup.find('meta', {'name' : 'dc.language'})['content']
-subj = soup.find('meta', {'name' : 'dc.subject'})['content']
-title = soup.find('meta', {'property' : 'og:title'})['content']
-datepub = soup.find('meta', {'name' : 'dcterms.created'})['content']
-
-## remove unnecessary elements
-# style
-for i in soup.find_all('style'):
-    i.decompose()
-
-# boiler plates
-for i in soup.find_all('section', {'class': re.compile('.*boilerplate.*')}):
-    i.decompose()
-
-# editor comments
-for i in soup.find_all('div', {'class': 'fbox'}):
-    i.decompose()
-
-# page numbers
-for i in soup.find_all('span', {'class': 'pagenum'}):
-    i.decompose()
-
-# remove br tags
-for i in soup.find_all('br'):
-    i.unwrap()
-
-# remove head
-soup.find('head').decompose()
+author = soup.find('meta', {'name' : 'AUTHOR'})
+lang = soup.find('meta', {'name' : 'dc.language'})
+subj = soup.find('meta', {'name' : 'dc.subject'})
+title = soup.find('meta', {'property' : 'og:title'})
+datepub = soup.find('meta', {'name' : 'dcterms.created'})
 
 
-# write to file with tags
-with open(f'input/book{book_id}.html', 'w', encoding = 'utf-8') as file:
+for i in [author, lang, subj, title, datepub]:
+    try:
+        i = i['content']
+    except:
+        i = 'NA'
+
+
+
+
+
+with open(f'input/{book_id}.html', 'w', encoding = 'utf-8') as file:
     file.write(str(soup.prettify()))
 
-with open(f'input/book{book_id}_fromhtml.txt', 'w', encoding = 'utf-8') as file:
-    file.write(soup.text)
